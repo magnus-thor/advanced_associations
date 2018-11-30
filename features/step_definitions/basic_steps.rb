0a46d067a4bdd84d0355d764e42fd0087d3f60f8
Magnus <magnus.thor.ar@gmail.com>
2018-11-30 16:10:49 +0100

    WIP adds models for user, post, likes, forum

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -1,12 +1,15 @@
 Given("these following users exists") do |table|
   table.hashes.each do |user|
-    create(user)
+    create(:user, user)
   end
 end
 
 Given("these following posts exists") do |table|
-  # table is a Cucumber::MultilineArgument::DataTable
-  pending # Write code here that turns the phrase above into concrete actions
+  table.hashes.each do | post |
+    forum = Forum.find_or_create_by(name: post[:forum])
+    user = User.find_by(email: post[:user])
+    create(:post, post.except("forum", "user", "likes").merge(forum: forum, user_id: user.id))
+  end
 end
 
 Given("I am logged in as {string}") do |string|
